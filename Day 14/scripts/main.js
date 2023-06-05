9df237677c747d87a4efc4ee29ed87e5e7771715
Kunmi A <113647454+kxn69@users.noreply.github.com>
2023-06-06 00:01:05 +0100

    completed day 14

---
+++ Day 14/scripts/main.js
@@ -0,0 +1,19 @@
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+// practice
+
+try {
+    const firstName = 'Ariyo'
+    const fullName = firstName + ' ' + lastName
+} catch (err) {
+    console.log(err)
+}
+
+
+// practice 2
+
+let num = 20
+console.log(num.toLowerCase())
+
+
