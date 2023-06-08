980d61329b242e639e41d46aaf4713dbd87d204e
Kunmi A <113647454+kxn69@users.noreply.github.com>
2023-06-08 20:06:18 +0100

    completed day 18 and level 1 of the exercise

---
+++ Day 18/readme.md
@@ -0,0 +1,5 @@
+## Day 18
+Today I learnt about Promise
+- How to create a promise using the promise constructor
+- fetching API
+- learnt about async and await in handling promises
---
+++ Day 18/scripts/main.js
@@ -0,0 +1,23 @@
+// Exercises: Level 1
+
+// 1. Read the countries API using fetch and print the name of country, capital, languages, population and area.
+const countriesAPI = 'https://restcountries.com/v2/all'
+
+const getCountriesData = async () => {
+    try {
+        const response = await fetch(countriesAPI)
+        const countries = await response.json()
+        countries.forEach(country => {
+            const { name, capital, languages, population, area } = country
+            console.log('Country:', name);
+            console.log('Capital:', capital);
+            console.log('Languages:', languages.map(lang => lang.name).join(', '));
+            console.log('Population:', population);
+            console.log('Area:', area);
+            console.log('=======================')
+        });
+    } catch (err) {
+        console.error(err)
+    }
+}
+getCountriesData()
